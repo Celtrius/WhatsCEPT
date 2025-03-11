0cbe55c960501104a32e3f8bab54a724d5ca141c
Celtrius <Celtrius@gmx.de>
2025-03-11 20:27:19 +0100

    Set base path for deployment and update asset references in dist/index.html

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Whatsapp-Chat-Export-Preview/",
   plugins: [react(), tailwindcss()],
 });
