498adb6b424e6aa8cac866352c45b6ff0c4bb971
Celtrius <Celtrius@gmx.de>
2025-03-11 20:30:27 +0100

    Update asset paths in index.html and remove base path from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Whatsapp-Chat-Export-Preview/",
   plugins: [react(), tailwindcss()],
 });
